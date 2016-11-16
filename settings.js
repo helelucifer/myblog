72db3d7b634eba3fc5d32c319fd6ce93c623f6d9
helelucifer <liyang857376354@outlook.com>
2016-11-16 21:00:48 +0800

    最新上传

---
+++ settings.js
@@ -0,0 +1,10 @@
+/**
+ * Created by 黎阳 on 2016/11/16.
+ */
+module.exports = {
+    cookieSecret:'lykey',
+    db:'myblog',
+host:'127.0.0.1',
+port:27017,
+url:"mongodb://127.0.0.1:27017/myblog"
+}
